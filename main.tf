3ac79b101cc2bab622abb6156ea5185874dd52d6
MeitalElisavetsky <142781353+MeitalElisavetsky@users.noreply.github.com>
2024-08-20 09:54:47 +0300

    Egress changes within the Public SG
    Allowed outbound HTTP traffic only.
    Before:
    It allowed unrestricted inbound traffic on ports 80 and 22 from anywhere on the internet. And it allowed all outbound traffic to anywhere on the internet.
    Now:
    Allows outbound traffic on port 80 to anywhere (potentially limiting this further could enhance security).

--- main.tf
+++ main.tf
@@ -119,9 +119,9 @@ resource "aws_security_group" "meital_public_sg" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
